35ca202f2e09b644bf305bb35acace1eb1940800
leam123 <garcia.leamor1@gmail.com>
2022-02-26 15:59:54 +0800

    update files

--- django_refresher/urls.py
+++ django_refresher/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shoemaker/', include('djoser.urls')),
     path('shoemaker/', include('djoser.urls.authtoken')),
-    path('/shoemaker/', include('product.urls')),
-    path('/shoemaker/', include('order.urls')),
+    path('shoemaker/', include('product.urls')),
+    path('shoemaker/', include('order.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
