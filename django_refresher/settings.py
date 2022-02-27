be62a0fadf04bc384067f681eb3ee7f9c96161cf
leam123 <garcia.leamor1@gmail.com>
2022-02-27 20:24:09 +0800

    Update settings.py

--- django_refresher/settings.py
+++ django_refresher/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -150,7 +151,8 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 
 STATIC_URL = 'https://shoemakerapi.herokuapp.com/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
